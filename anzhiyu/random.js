a70ffa0ee57121e98de397269b72917894b80174
Gaojc <2768308258@qq.com>
2023-12-15 21:13:08 +0800

    Site updated: 2023-12-15 21:13:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/09/25/Go题库/","2023/10/15/HTTP-队头阻塞/","2023/10/13/HTTP学习笔记/","2023/09/24/Linux命令总结/","2023/10/11/mysql笔记/","2023/09/24/保持专注/","2023/12/15/《MySQL是怎样运行的》笔记/"];function toRandomPost(){
+var posts=["2023/09/25/Go题库/","2023/10/15/HTTP-队头阻塞/","2023/10/13/HTTP学习笔记/","2023/10/11/mysql笔记/","2023/12/15/《MySQL是怎样运行的》笔记/","2023/09/24/Linux命令总结/","2023/09/24/保持专注/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
