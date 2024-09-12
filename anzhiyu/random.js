e10006528f661c46a664112a59746c11116dc6e8
gaojc1111 <2768308258@qq.com>
2024-09-12 16:44:20 +0800

    Site updated: 2024-09-12 16:44:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/10/15/HTTP队头阻塞/","2024/03/03/sync-Map源码解析/"];function toRandomPost(){
+var posts=["2023/09/24/Linux命令总结/","2023/10/15/HTTP队头阻塞/","2024/03/03/sync-Map源码解析/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
