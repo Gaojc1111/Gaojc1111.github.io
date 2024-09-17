55e0d0a939b990d7ff1dcdb22b6e4fed829beafd
gaojc1111 <2768308258@qq.com>
2024-09-17 22:17:29 +0800

    Site updated: 2024-09-17 22:17:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/09/24/Linux命令总结/","2024/09/14/Mysql中order-by是怎么工作的/","2023/10/15/HTTP队头阻塞/","2024/05/31/极客时间redis核心技术与实战笔记/","2024/03/03/sync-Map源码解析/","2024/09/12/算法笔记/"];function toRandomPost(){
+var posts=["2023/10/15/HTTP队头阻塞/","2024/09/14/Mysql中order-by是怎么工作的/","2024/03/03/sync-Map源码解析/","2023/09/24/Linux命令总结/","2024/09/12/算法笔记/","2024/05/31/极客时间redis核心技术与实战笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
